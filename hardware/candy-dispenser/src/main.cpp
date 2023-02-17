584101f85581362f24b095c0e236930c6a079981
hulohot <ethan@copiedcode.com>
2023-02-16 18:02:32 -0600

    Updated platformio project from Arduino Uno to ESP8266

--- hardware/candy-dispenser/src/main.cpp
+++ hardware/candy-dispenser/src/main.cpp
@@ -1,16 +1,14 @@
 #include <Arduino.h>
 #include <ESP8266WiFi.h>
-#include <ESP8266WebServer.h>
 #include <WiFiClient.h>
+#include <ESP8266WebServer.h>
 #include <Servo.h>
 
-// Replace with your network credentials
-const char* ssid = "BrickHouse";
-const char* password = "Olivia1Ethan2Jacob3";
+#include "secrets.h"
 
-// Set the IP address of the server you want to connect to
-const char* serverIP = "192.168.1.100";
-const int serverPort = 80;
+// Replace with your network credentials
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
 
 ESP8266WebServer server(80);
 Servo myservo;
@@ -24,17 +22,14 @@ void handleDispense() {
   Serial.print(duration);
   Serial.println(" seconds");
 
-
   // Move the servo to 45 degrees
   myservo.write(45);
-  digitalWrite(LED_BUILTIN, HIGH);
 
   // Wait for the specified number of seconds
   delay(duration * 1000);
 
   // Move the servo back to 0 degrees
   myservo.write(0);
-  digitalWrite(LED_BUILTIN, LOW);
 
   server.send(200, "text/plain", "Dispense complete");
 }
@@ -43,8 +38,6 @@ void setup() {
   Serial.begin(115200);
   myservo.attach(D3);
   myservo.write(0);
-  pinMode(LED_BUILTIN, OUTPUT);
-  digitalWrite(LED_BUILTIN, LOW);
   delay(1000);
 
   // Connect to Wi-Fi network
@@ -53,7 +46,8 @@ void setup() {
     delay(1000);
     Serial.println("Connecting to WiFi...");
   }
-
+  
+  // Print the local IP address of the ESP8266 board
   Serial.print("Connected to Wi-Fi. Local IP address: ");
   Serial.println(WiFi.localIP());
 
